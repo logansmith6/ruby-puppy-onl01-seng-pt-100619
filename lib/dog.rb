b3fbd7337d9773b64cfdd439027ba4e948e923bc
Logan Smith <logsmith06@gmail.com>
2019-11-12 15:26:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,16 +3,16 @@ class Dog
 
   @@all = []
   attr_accessor :name
-  
+
   def initialize(name)
     @name = name
     @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
-  
+
 
 
 
